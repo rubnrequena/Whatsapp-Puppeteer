aec980d423ab5a89d5683f68d3fb4a07507ad105
Ruben Requena <rubnrequena@gmail.com>
2019-05-05 10:15:42 -0400

    mejorar proceso de mensajes enviados

--- titere/ws.js
+++ titere/ws.js
@@ -16,26 +16,49 @@ var mensajes={};
 async function init() {
   if (!page) {
     page = await titere.newPage();      
-    initPage();
+    
     await page.goto('https://web.whatsapp.com/',{waitUntil:"networkidle2"});
     console.log("WS: Cargando...");    
     while (!await page.$('._1FKgS .app')) {
-      page.waitFor(1000);
+      page.waitFor(500);
       if (await page.$('.landing-main')) {
         log("Esperando validacion QR http://127.0.0.1/ws/qr");
         await page.screenshot({path:"public/images/lastQR.jpg"});
         page.waitFor(5000);
       }
     }
-    console.log("WS: Ready...");
+    console.log("WS: Ready...");    
+    initPage();
   }  
   setInterval(verificarMensajes,config.MSG_CHECK_DELAY);
 }
 async function initPage() {
   page.exposeFunction('msgCheck',mensajesEnviados);
   page.exposeFunction('newMsg',mensajesNuevos);
+  await page.evaluate(()=>{
+    let mut = new MutationObserver((muts) => {
+      muts.forEach(node => {
+        let span = node.target;
+        let uDiv = span.closest('._2EXPL');
+        let eNumero = uDiv.querySelector('._3TEwt > span:nth-child(1)');
+        if (eNumero) {
+          let nombre = eNumero.title;
+          let num = nombre.match(/(\d{1,3}) (\d{1,3})-(\d+)/);
+          if (num) {
+            num = num[1]+num[2]+num[3];
+          } else if (eNumero = uDiv.querySelector('img')) {        
+            if (num = /t=s&u=(\d+)/.exec(eNumero.src)) num = num[1];
+          } else num = nombre;
+          window.msgCheck(num,span.dataset.icon)
+          console.log(`Mensaje cambio de ${num}`);
+        }
+      })
+    })
+    let element = document.getElementById('pane-side');
+    mut.observe(element,{attributeFilter:["data-icon"],subtree:true});
+  })
 }
-function mensajesEnviados (num,msg,status) {
+function mensajesEnviados (num,status) {
   delete mensajes[num];
   switch (status) {
     case selector.statusCheck: 
@@ -112,25 +135,6 @@ async function typeMsg(num,msg) {
   await page.keyboard.up("ControlLeft");
   await page.keyboard.press("Enter");
 
-  await sentMsg(num);
-}
-async function sentMsg(num) { 
-  await page.waitForSelector('span[data-icon="status-time"]');
-  page.evaluate(num => {
-    let sending = document.querySelector('span[data-icon="status-time"]');      
-    sending.setAttribute("num",`${num}`);
-    var obs = new MutationObserver((mutations,observer) => {
-      let span = mutations[0].target;
-      let msg = span.parentElement.parentElement.title;
-      window.msgCheck(num,msg,span.dataset.icon);
-      if (span.dataset.icon=='status-dblcheck-ack') observer.disconnect();
-    })
-    obs.observe(sending,{attributes:true,subtree:true});
-  },num).catch(e=>{
-    console.log(clerror(e));
-    isSending=false;
-  });
-  console.log("mensaje enviado");
   isSending=false;
 }
 async function send(user,msg) { 
@@ -152,9 +156,9 @@ async function sendToNumber(num,msg) {
 
   page.close();
   page = apiPage;
-  initPage();
+  await initPage();
 
-  sentMsg(num);
+  isSending=false;
 }
 async function findUser (num) {
   let search = await page.$(selector.searchInput);
@@ -168,19 +172,16 @@ async function findUser (num) {
   }
   return false;
 }
-
 async function nextPending() {
   if (queueMsg.length>0) {
     let msg = queueMsg.shift();
     await send(msg.user,msg.msg);
   }
 }
-
 async function getScreen(name="lastQR",_page) {
   _page = _page || page;
   await _page.screenshot({path:`public/images/${name}.jpg`});
 }
-
 const selector = {
   mainPanel:"#pane-side",
   mainPanelInner:'.RLfQR',
